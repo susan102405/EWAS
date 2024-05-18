00e8cebf9c40407a5e60074ed7dfeff1035a4ada
susan102405 <48574785+susan102405@users.noreply.github.com>
2024-05-17 19:55:29 -0500

    Update Functions2.R

--- Functions2.R
+++ Functions2.R
@@ -332,7 +332,7 @@ f.GEE_LOGISTIC.par <- function(methcol, VAR, COV, ID, model_statement, datatype,
 }
 
 ## LMM-linear mixed model
-f.LMM.par <- function(methcol, VAR, COV, ID, model_statement, datatype, tdatRUN) { 
+f.LMMlite.par <- function(methcol, VAR, COV, ID, model_statement, datatype, tdatRUN) { 
   bigdata <- data.frame(na.omit(cbind(VAR = eval(parse(text = paste0("df$", VAR))), methy = tdatRUN[, methcol], COV, ID = ID)))  
   mod <- try(lmer(model_statement, data = bigdata))
   if("try-error" %in% class(mod)){
